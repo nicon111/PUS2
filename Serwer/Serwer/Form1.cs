a5156d68df0ed939238d6ff24b7adfbe2b836de1
Adam Swiercz <nicon111@interia.pl>
2011-11-08 13:13:36 +0100

    Program z watkami, przeslaniem danych i funkcja sprawdzajaca

--- Serwer/Serwer/Form1.cs
+++ Serwer/Serwer/Form1.cs
@@ -24,9 +24,18 @@ namespace Serwer
         private TcpListener serwer = null;
         private TcpClient klient = null;
 
+        private int sprawdzCzyMoznaZbudowacTrojkat(int []dlugosciBokow) 
+        {
+            Array.Sort(dlugosciBokow);
+            if(dlugosciBokow[0]+dlugosciBokow[1]>dlugosciBokow[2]){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+
         private void uruchom_Click(object sender, EventArgs e)
         {
-            
             IPAddress adresIP = null;
             String port = port2.Text;
             int porcik = System.Convert.ToInt16(port);
@@ -39,55 +48,74 @@ namespace Serwer
             }
             catch
             {
-                MessageBox.Show("Wystapił błąd z adrem IP");
-                listBox1.Text = String.Empty;
+                //MessageBox.Show("Wystapił błąd z adrem IP");
+                //listBox1.Text = String.Empty;
                 return;
             }
-
-            nawiazaniePolaczenia(porcik, adresIP);
+            serwer = new TcpListener(adresIP, porcik);
+            serwer.Start();
+            
+            while (true)
+            {
+                //listBox1.Items.Add("przed accept");
+                //listBox1.Refresh();
+                klient = serwer.AcceptTcpClient();
+                //listBox1.Items.Add("przed watkiem");
+                //listBox1.Refresh();
+                new Thread(() => this.wymianaDanych(klient)).Start();
+            }
 
         }
+        //*************************************************
+        /*private void wyswietlenieTekstu(ListBox listBox)
+        {
+            listBox.Items.Add(tekst);
+        }
+        public delegate void pobranieTekstu(String tekst);*/
+        //*************************************************
 
-        private void nawiazaniePolaczenia(int porcik, IPAddress adresIP)
+        private void wymianaDanych(TcpClient klient)
         {
             try
             {
-                    serwer = new TcpListener(adresIP, porcik);
-                    serwer.Start();
-                    klient = serwer.AcceptTcpClient();
-                    listBox1.Items.Add("Nawiazano polaczenie");
-                    listBox1.Refresh();
+               
+                // It's on a different thread, so use Invoke.
+                //SetTextCallback d = new SetTextCallback(SetText);
+                //listBox1.Invoke(new pobranieTekstu(this.wyswietlenieTekstu), new object[] { "nawiazano polaczenie" });
+                //listBox1.Items.Add("nawiazano polaczenie");
+                //this.Invoke (d, new object[] { "Nawiazanie połączenie"  });
 
-                    listBox1.Items.Add("Klient się podłączył.....");
-                    listBox1.Refresh();
-                    
-                    komunikatyOdKlienta(klient);
-                
+                //listBox1.Refresh();
             }
 
             catch (Exception ex)
             {
-                listBox1.Items.Add("Błąd połączenia");
-                MessageBox.Show(ex.ToString(), "Blad połączenia");
+                //listBox1.Items.Add("Błąd połączenia");
+                //MessageBox.Show(ex.ToString(), "Blad połączenia");
+            } 
+            int[] tab = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                BinaryReader read = new BinaryReader(klient.GetStream());
+                //BinaryWriter write = new BinaryWriter(stream); to bedzie potrzebna do odp
+                string dlugoscBoku = read.ReadString();
+                //listBox1.Items.Add(i + 1 + " Wiadmość od klienta...... " + dlugoscBoku);
+                Console.Write(i + 1 + " Wiadmość od klienta...... " + dlugoscBoku);
+
+                tab[i] = System.Convert.ToInt16(dlugoscBoku);
+               
+
+
+                //listBox1.Refresh();
             }
-        }
-
-        private void komunikatyOdKlienta(TcpClient klient)
-        {  
-                int []tab= new int[3];
-                for (int i = 0; i < 3; i++)
-                {
-                    BinaryReader read = new BinaryReader(klient.GetStream());
-                    //BinaryWriter write = new BinaryWriter(stream); to bedzie potrzebna do odp
-                    string dlugoscBoku = read.ReadString();
-                    listBox1.Items.Add(i+1 + " Wiadmość od klienta...... " + dlugoscBoku);
-
-                    tab[i] = System.Convert.ToInt16(dlugoscBoku);
-                    
-                    listBox1.Refresh();
-                }
-            
-            
+            if (sprawdzCzyMoznaZbudowacTrojkat(tab) == 1)
+            {
+                Console.Write("Mozna zbudowac trojkat");
+            }
+            else
+            {
+                Console.Write("nie mozna zbudowac trujkata");
+            }
         }
 
         private void stop_Click(object sender, EventArgs e)
@@ -95,7 +123,7 @@ namespace Serwer
             serwer.Stop();
             klient.Close();
 
-            listBox1.Items.Add("Serwer zakończył prace....");
+            //listBox1.Items.Add("Serwer zakończył prace....");
             uruchom.Enabled = true;
             stop.Enabled = false;
         }
